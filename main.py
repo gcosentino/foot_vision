59cfac6885e17face95aa292cc0617fbb93abdaf
gcosentino <cosentino.ferreira@gmail.com>
2024-05-25 20:02:37 -0400

    tracker script updated + pickle stub

--- main.py
+++ main.py
@@ -9,7 +9,15 @@ def main():
     # Initialize Tracker
     tracker = Tracker('models/best.pt')
 
-    tracks = tracker.get_object_tracks(video_frames)
+    tracks = tracker.get_object_tracks(video_frames,
+                                       read_from_stub=True,
+                                       stub_path='stubs/track_stubs.pkl')
+    
+    # Draw Output
+
+    # Draw object tracks
+
+    output_video_frames = tracker.draw_annotations(video_frames, tracks)
     # Save Video
     save_video(video_frames, 'output_videos/output_video.avi')
 
