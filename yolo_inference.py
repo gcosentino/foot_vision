46eded018ad64c74f23f99ae592f58c90c9f9274
gcosentino <cosentino.ferreira@gmail.com>
2024-05-27 01:57:14 -0400

    updates + color assignment start

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 #model = YOLO('yolov8x')
-model = YOLO('models/best.pt')
+model = YOLO('models/new_best.pt')
 
 
 results = model.predict('input_videos/08fd33_4.mp4', save=True)
